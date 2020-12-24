3db7dea505e00938f59a2702c4bd89b072958164
Denllay <denllaay@gmail.com>
2020-12-24 13:01:39 +0300

    Add files via upload

---
+++ webpack.base.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.bundle\.ts$/,
+        use: {
+          loader: "bundle-loader",
+          options: {
+            name: "[name]",
+          },
+        },
+      },
+      // Тупескрипт
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      // Картинки
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      // Шрифты
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      // Сксс
+      {
+        test: /\.scss|\.sass$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                outputStyle: "compressed",
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    // Очистка говна
+    new CleanWebpackPlugin(),
+  ],
+};
